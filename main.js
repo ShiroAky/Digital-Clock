106d3a27a2e1ee3854086dc7ff5c16adf2c4859f
ShiroAky <73247790+ShiroAky@users.noreply.github.com>
2022-07-09 18:55:04 -0400

    Update

--- main.js
+++ main.js
@@ -7,22 +7,8 @@ const horas = document.getElementById('hora');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
-// window.setInterval(
-
-//     function values() {
-
-//         btnInput.addEventListener('input', (e) => {
-
-//             show.innerHTML = e.target.value;
-
-//         });
-
-//     }, 1000);
-
-
 window.setInterval(
     function showTime() {
-
         const dia = new Date();
         const hora = new Date();
         const minuto = new Date();
@@ -32,5 +18,4 @@ window.setInterval(
         horas.innerHTML = hora.getHours();
         minutos.innerHTML = minuto.getMinutes();
         segundos.innerHTML = segundo.getSeconds();
-
-    }, 1000);
+}, 1000);
